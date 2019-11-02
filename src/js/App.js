90d0bc1d129de223dae85f51b69329903d42b00b
tannermwilson <tanner.mike.wilson@gmail.com>
2019-11-02 16:29:00 -0400

    update js structure

--- src/js/App.js
+++ src/js/App.js
@@ -5,29 +5,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Customer(id) {
-  let data = new Data(id);
+  this.data = new Data(id);
 }
 
 function Data(id){
-  let monthData = [];
+  this.monthData = [];
   let year;
   let month;
-  let
   for (year = 8; year <= 9; year++){
     for (month = 1; month <= 12; month++){
-      monthData.push(new Month(id, year, month));
+      this.monthData.push(new Month(id, year, month));
+      if (this.monthData[month].dayData[0].energy.length == 0){
+        this.monthData.pop();
+      }
     }
   }
 }
 
 function Month(id, year, month){
-  let dayData = [];
+  this.dayData = [];
   let day;
   for (day = 1; day <= 31; day++){
-    dayData.push(new Day(id, year, month, day));
+    this.dayData.push(new Day(id, year, month, day));
+    if (this.dayData[day-1].energy.length == 0){
+      this.dayData.pop();
+    }
   }
 }
 
 function Day(id, year, month, day){
-
+  this.energy = [];
+  // add energy to array, if that day exists
 }
