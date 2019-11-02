f2f002e60ef34e4beb7c30e8edf0f95c405fea0e
tannermwilson <tanner.mike.wilson@gmail.com>
2019-11-02 16:14:03 -0400

    add js structure

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
 import logo from '../logo.svg';
 import '../css/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-function App() {
-  return ();
+function Customer(id) {
+  let data = new Data(id);
 }
 
-export default App;
+function Data(id){
+  let monthData = [];
+  let year;
+  let month;
+  let
+  for (year = 8; year <= 9; year++){
+    for (month = 1; month <= 12; month++){
+      monthData.push(new Month(id, year, month));
+    }
+  }
+}
+
+function Month(id, year, month){
+  let dayData = [];
+  let day;
+  for (day = 1; day <= 31; day++){
+    dayData.push(new Day(id, year, month, day));
+  }
+}
+
+function Day(id, year, month, day){
+
+}
